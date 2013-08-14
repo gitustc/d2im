2c76948dd9e1edd50634379015cd41eb5b218ab8
anhong <anhong@deb7>
2013-08-14 02:12:49 -0400

    add crc calculation

--- src/d2lib/d2pak/d2pak.c
+++ src/d2lib/d2pak/d2pak.c
@@ -1285,6 +1285,24 @@ static uint32_t glb_encry_table[1280] = {
 
 
 
+DWORD Crc(char *string,DWORD *massive_base,DWORD massive_base_offset)
+{
+    char   byte;
+    DWORD  crc=0x7fed7fed;
+    DWORD  s1=0xEEEEEEEE;
+
+    byte=*string;
+    while(byte) {
+        if(byte>0x60 && byte<0x7B)
+            byte-=0x20;
+        //massive_base¿?¿?¿?¿?¿?¿?¿?¿?¿?¿?¿?¿?
+        crc=*(massive_base+massive_base_offset+byte)^(crc+s1);
+        s1+=crc+(s1<<5)+byte+3;
+        string++;
+        byte=*string;
+    }
+    return crc;
+}
 
 
 
